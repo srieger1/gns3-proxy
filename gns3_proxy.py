bf6716f35c3c4eca245194c523e3250b13291553
vasanth350 <79219201+vasanth350@users.noreply.github.com>
2021-06-09 20:58:33 +0530

    update syntax error
    --log-level = config.get('proxy','log-level')

--- gns3_proxy.py
+++ gns3_proxy.py
@@ -1082,9 +1082,9 @@ def main():
     #Both upper and lowercase values are allowed
     #You may also simply use the leading character e.g. --log-level d
     if config.get('proxy','log-level'):
-    	--log-level = config.get('proxy','log-level')
+    	log-level = config.get('proxy','log-level')
     else:
-    	--log-level = "INFO"
+    	log-level = "INFO"
     	
     #get inactivity time limit
     #
